8fd5ae70ad78c7e4bc5d0366ff0b0251cda5a863
Derek Duoba <derekduoba@gmail.com>
2015-03-04 13:31:48 -0500

    added code for performSeletion, performCrossover, and mutateChild

--- Driver.java
+++ Driver.java
@@ -4,81 +4,118 @@ import java.lang.Math;
 
 public class Driver{
 
-    PriorityQueue<Candidate> fitnessHeap;
+    //PriorityQueue<Candidate> fitnessHeap;
+    Random random;
+    int generationCount = 1;
 
-    /*
-	static boolean compareObj(Candidate obj){
+	static boolean checkResult(Candidate obj){
 		//temporarily 5,000,000; will eventually be ~10,000,000 - 25
 		if( (-(Math.pow(2, (obj.x - 3750))) + (570 * obj.x) - 
-			 2 * (Math.pow(2, (obj.y - 5000))) + (1500 * obj.y)) >= 5000000){
-			System.out.println("You Win!!\n" + "X = " + obj.x + " Y = " + obj.y); 
+			 2 * (Math.pow(2, (obj.y - 5000))) + (1500 * obj.y)) >= 9999975){
+			System.out.println("You Win!!\n" + "X = " + obj.x + " Y = " + obj.y + " Generation = " generationCount); 
 			return true;
 		}else{
 			return false;
 		}
 	}
-    */
 
+
+    // we might not need this method
     static int determineFitness(Candidate currentCandidate) {
     
             // determine + set a candidate's fitness score
-            
+               
+
             // add the current candidate to the fitness heap
     
     }
 
-    static void performSelection(Candidate currentCandidate) {
+    static void performSelection(Candidate currentCandidate, int index) {
+        Math random = new Random();
+        int n = (100 - index);
+        int m = random.nextInt(100);
 
-        int n = 1;
         if ((n+m) <= 100) {
             // pair with index m + n
+            performCrossover(currentCandidate, fitnessList.get(m+n);
         } else {
             // pair with neighbor
+            if (i > 0) {
+                performCrossover(currentCandidate, fitnessList.get(i - 1);
+            } else {
+                performCrossover(currentCandidate, fitnessList.get(99); // started from the bottom now we here
+            }
         }
-
-        // performCrossover();
 
     }
 
     static void performCrossover(Candidate parentOne, Candidate parentTwo) {
+        Candidate childOne;
+        Candidate childTwo;
 
         // make babies
         
         // mutate babies
+        mutateChild(childOne);
+        mutateChild(childTwo);
 
         // add mutated babies to uniqueCandidate
+        
     }
 
     static void mutateChild(Candidate currentChild) {
         
         // add a random number between -50 and 50
-        
+        int mutateValue = random.nextInt(100) - 50;
+        int choice = random.nextInt(1);
+        if (choice) {
+            currentChild.x += mutateValue;
+        } else {
+            currentChild.y += mutateValue;
+        }
     }
 
 	public static void main(String[] args){
-		Candidate uniqueCandidate[] = new Candidate[100];
-		/*int iterations = 1;*/
-		int i = 0, j = 1, xMax1 = 0, xMax2 = 0, yMax1 = 0, yMax2 = 0;
-		Random math = new Random();
+		Candidate uniqueCandidate[] = new Candidate[100]; 
+		ArrayList<Candidate> fitnessList[] = new ArrayList<Candidate>(100);
+        //fitnessHeap = new PriorityQueue<Candidate>(100);
 
-		//initializing population_0
-		//need something to handle duplicates
+        /*int iterations = 1;*/
+		
+        int i = 0, j = 1, xMax1 = 0, xMax2 = 0, yMax1 = 0, yMax2 = 0;
+        //random = new Random();
+
+		// it may be of use to need something to handle duplicates (in order to vary the population better)
 		for(int x = 0; x < 100; x++){
 			uniqueCandidate[x] = new Candidate();
 		}
 
 		while(true){
-			for(int x = 0; x < 100; x++){
-				//Checks to see if solution is found
-				if(compareObj(uniqueCandidate[x])){break;}
-				//Add elements to max_heap too!
+			
+            for(int x = 0; x < 99; x++){
+				
+                //Checks to see if solution is found
+				if(checkResult(uniqueCandidate[x])) { break; }
+				
+                //Add elements to the fitness list too!
+                fitnessList.add(uniqueCandidate[x]);
+                uniqueCandidate[x] = null;
+
 			}
-			//i handles odd parents
+			
+            while (i < 99) {
+                performCrossover();
+            }
+
+            //i handles odd parents
 			//j handles even parents
-			while(i < 50){
+            /*
+            while(i < 50){
 				//variables to be used in this loop
-				xMax1 = uniqueCandidate[i].x; yMax1 = uniqueCandidate[i].y;
-				xMax2 = uniqueCandidate[j].x; yMax2 = uniqueCandidate[i].y;
+				xMax1 = uniqueCandidate[i].x;
+                yMax1 = uniqueCandidate[i].y;
+				xMax2 = uniqueCandidate[j].x; 
+                yMax2 = uniqueCandidate[j].y;
 
 				if(xMax1 > xMax2){
 					if(yMax1 > yMax2){
@@ -103,6 +140,7 @@ public class Driver{
 				//increment by two to only deal with odd/even parents
 				i += 2; j += 2;
 			}
+            */
 		}
 	}
 }
